5d02b07887abfb4fb0456031d6306bc743733daf
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-03-05 18:00:33 +0200

    stripe integration

--- types.ts
+++ types.ts
@@ -30,7 +30,7 @@ export interface Product {
 
 export interface Price {
   id: string;
-  prodcut_id?: string;
+  product_id?: string;
   active?: boolean;
   description?: string;
   unit_amount?: number;
