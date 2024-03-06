1dc58b70c20fea0baeec0761b185bc7a952609f7
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-03-06 15:18:13 +0200

    modified useUploadModal

--- hooks/UseUploadModal.ts
+++ hooks/UseUploadModal.ts
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import create from "zustand";
 
 interface UploadModalStore {
   isOpen: boolean;
@@ -6,10 +6,16 @@ interface UploadModalStore {
   onClose: () => void;
 }
 
-const useUploadModal = create<UploadModalStore>((set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
-}));
+type SetState<T> = (
+  newState: Partial<T> | ((prevState: T) => Partial<T> | null)
+) => void;
+
+const useUploadModal = create<UploadModalStore>(
+  (set: SetState<UploadModalStore>) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
+  })
+);
 
 export default useUploadModal;
