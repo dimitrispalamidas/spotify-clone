a4f471bc59139df7ef5171c593388264dd7c36e1
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-03-06 22:01:44 +0200

    fixing build erros 2.0

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ytydhwzrlavqkbknxfxq.supabase.co"],
+    domains: [
+      "misc.scdn.co",
+      "i.scdn.co",
+      "geo-media.beatsource.com",
+      "i1.sndcdn.com",
+      "media.pitchfork.com",
+      "seed-mix-image.spotifycdn.com",
+      "ytydhwzrlavqkbknxfxq.supabase.co",
+    ],
   },
 };
 
